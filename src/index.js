4170eeadc7e5306308de7e4a715e9ffaa5c9b04e
채민 <sonchaemin89@sookmyung.ac.kr>
2024-08-11 19:15:50 +0900

    branch에 올리는 방법 테스트

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//하하 테스트 입니다람쥐
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
