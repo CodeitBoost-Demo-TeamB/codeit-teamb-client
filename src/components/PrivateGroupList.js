582b48290fa13ab4c818916f7cbf27f42c307e84
지은 <danashin02@sookmyung.ac.kr>
2024-08-12 10:59:20 +0900

    비공개 목록 구현

--- src/components/PrivateGroupList.js
+++ src/components/PrivateGroupList.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import './PrivateGroupList.css';
+import Header from '../components/Header';
+import GroupList from '../components/GroupList';
 
 function PrivateGroupList() {
-  const privateGroups = ['Private Group 1', 'Private Group 2', 'Private Group 3']; // 임의의 데이터
+  const groups = [
+    {
+      id: 1,
+      name: '달팽이의 가족',
+      description: '그룹 설명이 여기에 들어갑니다.',
+      d: 265,
+      status: '비공개',
+      comments: 8,
+      views: '1.5K',
+      likes: 15
+    },
+    // 추가 그룹 데이터
+  ];
 
   return (
     <div className="private-group-list">
-      <h1>비공개 그룹 목록</h1>
-      <ul>
-        {privateGroups.map((group, index) => (
-          <li key={index}>{group}</li>
-        ))}
-      </ul>
+      <Header />
+      <GroupList groups={groups} />
+      <div className="load-more">
+        <button className="btn load-more-btn">더보기</button>
+      </div>
     </div>
   );
 }
