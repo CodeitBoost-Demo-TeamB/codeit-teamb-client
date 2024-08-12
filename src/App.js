1e92f1a7276dcc94c52c3082f198c0979e33ba65
지은 <danashin02@sookmyung.ac.kr>
2024-08-12 10:15:06 +0900

    기본 구조 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupAccess from './components/GroupAccess';
+import CommentDeleteModal from './components/CommentDeleteModal';
+import CommentRegisterModal from './components/CommentRegisterModal';
+import CommentEditModal from './components/CommentEditModal';
+import NotFoundPage from './components/NotFoundPage';
+import PublicGroupList from './components/PublicGroupList';
+import PrivateGroupList from './components/PrivateGroupList';
+import EmptyPublicGroupList from './components/EmptyPublicGroupList';
+import CreateGroup from './components/CreateGroup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={PublicGroupList} />
+        <Route path="/private-groups" component={PrivateGroupList} />
+        <Route path="/empty-public-groups" component={EmptyPublicGroupList} />
+        <Route path="/create-group" component={CreateGroup} />
+        <Route path="/group-access" component={GroupAccess} />
+        <Route path="/comment-delete" component={CommentDeleteModal} />
+        <Route path="/comment-register" component={CommentRegisterModal} />
+        <Route path="/comment-edit" component={CommentEditModal} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
 
